3e9df461f1e8ad72bc77499719fe111f2f47d980
Bernhard M. Wiedemann <bwiedemann@suse.de>
2018-09-12 13:10:29 +0000

    Use smarter diff calculation
    so that visible changes are roughly equal at all levels

--- etc/acpi/acer-tm-brightup.sh
+++ etc/acpi/acer-tm-brightup.sh
@@ -2,7 +2,7 @@
 
 max=$(cat /sys/class/backlight/intel_backlight/max_brightness)
 curr=$(cat /sys/class/backlight/intel_backlight/actual_brightness)
-div=$((max / 10))
+div=$((max / 300 + curr / 5))
 next=$((curr+div))
 if [ $next -lt $max ]; 
 then
